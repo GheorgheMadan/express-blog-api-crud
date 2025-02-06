d63bc852a3164c2a0565c171e7e5488f820e6fdb
Gheorghe <madangheorghe.30@gmail.com>
2025-02-06 16:34:54 +0100

    create destroy function

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -36,9 +36,37 @@ function update(req, res) {
 
 }
 
+// funzione delete che eliminerà un singolo post dalla lista 
 function destroy(req, res) {
+    // Converto il parametro ID in un numero intero 
+    const id = parseInt(req.params.id);
 
-}
+    // Cerco il post con l'ID corrispondente all'array
+    const post = posts.find(post => post.id === id)
+
+    // Se il post non viene trovato 
+    if(!post){
+
+        //imposto lo status 404
+        res.status(404)
+
+         // restituisco un messaggio di errore (formato json)
+         return res.json({
+            error: "Not Found", // Tipo di errore 
+            message:"Post non trovato" // descrizione del problema
+        })
+    };
+
+    // elimino il post 
+    posts.splice(posts.indexOf(post), 1); // cancello 1 elemento
+    
+    // stampo in console l'array per confrontare il risultato 
+    console.log(posts);
+
+    // imposto lo status a 204 modifica avvenuta
+    res.sendStatus(204) 
+    
+};
 
 // esportiamo tutto
 module.exports = { index, show, store, update, destroy }
