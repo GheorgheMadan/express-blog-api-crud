01a7ad69f3cfacee67c2978129884a1aeb666f76
Gheorghe <madangheorghe.30@gmail.com>
2025-02-06 16:55:17 +0100

    bonus

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -1,10 +1,15 @@
 //importo i dati dei posts 
 const posts = require('../data/postsData.js');
 
-function index(req, res) {
-// copiamo la logica dell'index
-    res.json(posts)
-}
+// FUNZIONE CHE CONTIENE UN FILTRO QUESOTO MOSTRA SOLO I POST CHE HANNO UN DETERMINATO TAG 
+    function index(req, res) {
+        let filteredPosts = posts
+        if(req.query.tag){
+           filteredPosts = posts.filter(post => post.tags.includes(req.query.tag)) 
+        };
+        res.json(filteredPosts);
+    }
+
 
 // FUNZIONE SHOW CHE MI MOSTRA SOLO L'OGGETTO CHE CONTIENTE UN DETERMINATO ID
 function show(req, res) {
@@ -28,45 +33,38 @@ function show(req, res) {
     res.json(post);
 }
 
-function store(req, res) {
-
-}
-
-function update(req, res) {
-
-}
-
 // funzione delete che eliminerà un singolo post dalla lista 
-function destroy(req, res) {
-    // Converto il parametro ID in un numero intero 
-    const id = parseInt(req.params.id);
+    function destroy(req, res) {
+        // Converto il parametro ID in un numero intero 
+        const id = parseInt(req.params.id);
 
-    // Cerco il post con l'ID corrispondente all'array
-    const post = posts.find(post => post.id === id)
+        // Cerco il post con l'ID corrispondente all'array
+        const post = posts.find(post => post.id === id)
 
-    // Se il post non viene trovato 
-    if(!post){
+        // Se il post non viene trovato 
+        if(!post){
 
-        //imposto lo status 404
-        res.status(404)
+            //imposto lo status 404
+            res.status(404)
 
-         // restituisco un messaggio di errore (formato json)
-         return res.json({
-            error: "Not Found", // Tipo di errore 
-            message:"Post non trovato" // descrizione del problema
-        })
+            // restituisco un messaggio di errore (formato json)
+            return res.json({
+                error: "Not Found", // Tipo di errore 
+                message:"Post non trovato" // descrizione del problema
+            })
+        };
+
+        // elimino il post 
+        posts.splice(posts.indexOf(post), 1); // cancello 1 elemento
+        
+        
+        // stampo in console l'array per confrontare il risultato 
+        console.log(posts);
+
+        // imposto lo status a 204 modifica avvenuta
+        res.sendStatus(204) 
+        
     };
 
-    // elimino il post 
-    posts.splice(posts.indexOf(post), 1); // cancello 1 elemento
-    
-    // stampo in console l'array per confrontare il risultato 
-    console.log(posts);
-
-    // imposto lo status a 204 modifica avvenuta
-    res.sendStatus(204) 
-    
-};
-
 // esportiamo tutto
-module.exports = { index, show, store, update, destroy }
+module.exports = { index, show, destroy }
--- routers/postsRouters.js
+++ routers/postsRouters.js
@@ -13,17 +13,6 @@ router.get('/', controllers.index);
 // show leggo solo un elemento in base al suo id 
 router.get('/:id', controllers.show); 
     
-// store creazione di un elemento 
-router.post('/', controllers.store)
-
-// put modifica integrale 
-router.put('/:id', controllers.update)
-
-// // modify modifica del elemento 
-// router.patch('/:id', (req, res)=>{
-//     res.send(`modifica dell'elemento con l'id: ${req.params.id}`)
-// });
-
 // delete eliminazione del elemento
 router.delete('/:id', controllers.destroy)
 
